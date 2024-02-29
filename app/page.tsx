03f099c592a4f57e246985211bf24c7757090202
sushant <raosushant045@gmail.com>
2024-02-29 22:50:29 +0530

    about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,19 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
     <>
-      <div className="bg  w-[1512px] ">
-        <div className="flex justify-center items-center text-white">
-          hi
+      <div className="bg w-[1512px] ">
+        <div className="border-[#604897] border-2 w-full">
+          <div className="w-full h-full bg-black opacity-55">
+            <div className=""></div>
+            <div>
+
+            </div>
+          </div>
         </div>
-        {/* <Image
-          src="/Gradient.svg"
-          alt="gradient img"
-          width={1512}
-          height={2046}
-          className="realative opacity-100"
-        ></Image> */}
       </div>
-      <div className=" text-white">hi</div>
     </>
   );
 }

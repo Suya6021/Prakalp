5582c72f1e27899578a08bdd698ca2cc12644fc0
sushant <raosushant045@gmail.com>
2024-03-04 15:52:05 +0530

    Ui Updates

--- components/Prize.tsx
+++ components/Prize.tsx
@@ -9,7 +9,7 @@ const Prize = () => {
     <div
       className={twJoin(
         lexend.className,
-        "w-full mt-[180px] min-h-full pb-10 md:px-0"
+        "w-full mt-[180px] min-h-full pb-28 md:px-0"
       )}
     >
       <div className="md:ml-28">
@@ -31,14 +31,14 @@ const Prize = () => {
             Prizes for Winner's
           </h3>
         </div>
-        <div className="flex gap-12 mt-[80px] w-full justify-center md:justify-around items-center">
+        <div className="flex gap-4 md:gap-12 mt-[80px] w-full justify-center md:justify-around items-center">
           <div className="flex flex-col justify-center items-center p-2 ">
             <Image
               src="./medal.svg"
               alt="medal icon"
               width={100}
               height={100}
-              className="w-[70px] md:w-[150px]"
+              className="w-[50px] md:w-[150px]"
             ></Image>
             <p className="lg:text-xl ">2nd Prize</p>
             <h4 className="xl lg:text-3xl whitespace-nowrap">upto 10,000/-</h4>
@@ -50,7 +50,7 @@ const Prize = () => {
               alt="medal icon"
               width={100}
               height={100}
-              className="w-[70px] md:w-[150px]"
+              className="w-[50px] md:w-[150px]"
             ></Image>
             <p className="lg:text-xl">1nd Prize</p>
             <h4 className="xl lg:text-3xl whitespace-nowrap">upto 25,000/-</h4>
@@ -61,13 +61,13 @@ const Prize = () => {
               alt="medal icon"
               width={100}
               height={100}
-              className="w-[70px] md:w-[150px]"
+              className="w-[50px] md:w-[150px]"
             ></Image>
             <p className="lg:text-xl">3rd Prize</p>
             <h4 className="xl lg:text-3xl whitespace-nowrap">upto 5,000/-</h4>
           </div>
         </div>
-        <div className="md:text-left text-center text-lg mt-20">
+        <div className="md:text-left text-center md:text-xl tracking-wider mt-20">
           <h5>Note : Winners will be selected from all the tracks(domains)</h5>
         </div>
       </div>

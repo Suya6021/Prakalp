ceffad169ed20150b3a787b23cc288d98ef8f040
sushant <raosushant045@gmail.com>
2024-03-05 12:51:47 +0530

    Timer Updates

--- components/CountDown.tsx
+++ components/CountDown.tsx
@@ -1,69 +1,60 @@
-'use client'
-import React from 'react'
-import { useEffect, useState } from 'react'
-import { TimerContainer } from './TimerContainer';
-
-import { twJoin } from 'tailwind-merge';
+"use client";
+import React, { useEffect, useState } from "react";
+import { TimerContainer } from "./TimerContainer";
 
 const CountDown = () => {
+  const destinationDate = new Date("2024-03-20T00:00:00"); // Set the destination date to March 20th at midnight UTC
+  const [days, setDays] = useState<number>(0);
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
+  const [message, setMessage] = useState<string>("");
 
-    const [time, setTime] = useState<number>(7);
-    const [newTime, setNewTime] = useState<number>(0)
-    const [days, setDays] = useState<number>(0);
-    const [hours, setHours] = useState<number>(0);
-    const [minutes, setMinutes] = useState<number>(0);
-    const [seconds, setSeconds] = useState<number>(0);
-    const [message, setMessage] = useState<string>("");
-  
-    const timeToDays = time * 60 * 60 * 24 * 1000;
-  
-    let countDownDate = new Date().getTime() + timeToDays;
-  
-  
-    useEffect(() => {
-  
-  
-      var updateTime = setInterval(() => {
-        var now = new Date().getTime();
-  
-        var difference = countDownDate - now;
-  
-        var newDays = Math.floor(difference / (1000 * 60 * 60 * 24));
-        var newHours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var newMinutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        var newSeconds = Math.floor((difference % (1000 * 60)) / 1000);
-  
-        setDays(newDays);
-        setHours(newHours);
-        setMinutes(newMinutes);
-        setSeconds(newSeconds);
-  
-  
-        if (difference <= 0) {
-          clearInterval(updateTime);
-          setMessage("The Launch Has Started");
-          setDays(0);
-          setHours(0);
-          setMinutes(0);
-          setSeconds(0);
-        }
-      })
-  
-      return () => {
+  useEffect(() => {
+    const updateTime = setInterval(() => {
+      const now = new Date();
+
+      const difference = destinationDate.getTime() - now.getTime();
+
+      const newDays = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const newHours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const newMinutes = Math.floor(
+        (difference % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      const newSeconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+      setDays(newDays);
+      setHours(newHours);
+      setMinutes(newMinutes);
+      setSeconds(newSeconds);
+
+      if (difference <= 0) {
         clearInterval(updateTime);
+        setMessage("The Launch Has Started");
+        setDays(0);
+        setHours(0);
+        setMinutes(0);
+        setSeconds(0);
       }
-  
-    }, [time]);
+    }, 1000);
+
+    return () => {
+      clearInterval(updateTime);
+    };
+  }, []); // Removed 'time' from the dependency array as it's constant
+
   return (
     <div>
-       <TimerContainer
+      <TimerContainer
         days={days}
         hours={hours}
         minutes={minutes}
         seconds={seconds}
       />
     </div>
-  )
-}
+  );
+};
 
-export default CountDown
+export default CountDown;
